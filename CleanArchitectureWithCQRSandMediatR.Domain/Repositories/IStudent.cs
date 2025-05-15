56b09c0d73c306d1eae07f68a4113023296b2356
IroshanaRavishan <bandarairoshana@gmail.com>
2025-05-15 18:30:56 +0530

    added Student entity and repository in domain project

---
+++ CleanArchitectureWithCQRSandMediatR.Domain/Repositories/IStudent.cs
@@ -0,0 +1,21 @@
+﻿using CleanArchitectureWithCQRSandMediatR.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitectureWithCQRSandMediatR.Domain.Repositories
+{
+    public interface IStudent
+    {
+        Task<List<Student>> GetAllStudentAsync();
+        Task<Student> GetStudentByIdAsync(int id);
+        Task<Student> GetStudentByNameAsync(string name);
+        Task<Student> GetStudentByEmailAsync(string email);
+        Task<Student> CreateAsync(Student student);
+        Task<int> UpdateAsync(int id, Student student);
+        Task<int> DeleteAsync(int id);
+    }
+}
+
