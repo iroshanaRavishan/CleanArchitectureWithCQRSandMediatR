48e683da7c9f044a028aa5b22f4042eb39064452
IroshanaRavishan <bandarairoshana@gmail.com>
2025-05-15 22:33:25 +0530

    added the feature to get the student by name

---
+++ CleanArchitectureWithCQRSandMediatR.Application/Students/Queries/GetStudentByName/GetStudentByNameQuery.cs
@@ -0,0 +1,15 @@
+﻿using CleanArchitectureWithCQRSandMediatR.Application.Students.Queries.GetStudentById;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitectureWithCQRSandMediatR.Application.Students.Queries.GetStudentbyName
+{
+    public class GetStudentByNameQuery : IRequest<StudentViweModel>
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ CleanArchitectureWithCQRSandMediatR.Application/Students/Queries/GetStudentByName/GetStudentByNameQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using CleanArchitectureWithCQRSandMediatR.Application.Students.Queries.GetStudentById;
+using CleanArchitectureWithCQRSandMediatR.Application.Students.Queries.GetStudentbyName;
+using CleanArchitectureWithCQRSandMediatR.Domain.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitectureWithCQRSandMediatR.Application.Students.Queries.GetStudentByName
+{
+    public class GetStudentByNameQueryHandler : IRequestHandler<GetStudentByNameQuery, StudentViweModel>
+    {
+        private readonly IStudentRepository _studentRepository;
+        private readonly IMapper _mapper;
+
+        public GetStudentByNameQueryHandler(IStudentRepository studentRepository, IMapper mapper)
+        {
+            _studentRepository = studentRepository;
+            _mapper = mapper;
+        }
+        public async Task<StudentViweModel> Handle(GetStudentByNameQuery request, CancellationToken cancellationToken)
+        {
+            var student = await _studentRepository.GetStudentByNameAsync(request.Name);
+            return _mapper.Map<StudentViweModel>(student);
+        }
+    }
+}
