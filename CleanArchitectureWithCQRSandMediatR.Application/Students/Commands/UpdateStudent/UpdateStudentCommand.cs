b8f1ce0b2b0459f63217eced447d8efdbce727e7
IroshanaRavishan <bandarairoshana@gmail.com>
2025-05-15 22:16:09 +0530

    completed the coding part of the Update student command and its handler

---
+++ CleanArchitectureWithCQRSandMediatR.Application/Students/Commands/UpdateStudent/UpdateStudentCommand.cs
@@ -0,0 +1,33 @@
+﻿using CleanArchitectureWithCQRSandMediatR.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitectureWithCQRSandMediatR.Application.Students.Commands.UpdateStudent
+{
+    public class UpdateStudentCommand : IRequest<int>
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+        public string GuardianName { get; set; }
+        public string GuardianPhone { get; set; }
+        public string GuardianEmail { get; set; }
+        public string RelationshipToStudent { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
---
+++ CleanArchitectureWithCQRSandMediatR.Application/Students/Commands/UpdateStudent/UpdateStudentCommandHandler.cs
@@ -0,0 +1,47 @@
+﻿using CleanArchitectureWithCQRSandMediatR.Domain.Entities;
+using CleanArchitectureWithCQRSandMediatR.Domain.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitectureWithCQRSandMediatR.Application.Students.Commands.UpdateStudent
+{
+    public class UpdateStudentCommandHandler : IRequestHandler<UpdateStudentCommand, int>
+    {
+        private readonly IStudentRepository _studentRepository;
+
+        public UpdateStudentCommandHandler(IStudentRepository studentRepository)
+        {
+            _studentRepository = studentRepository;
+        }
+        public async Task<int> Handle(UpdateStudentCommand request, CancellationToken cancellationToken)
+        {
+            var studentEntity = new Student()
+            {
+                Id = request.Id,
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Gender = request.Gender,
+                DateOfBirth = request.DateOfBirth,
+                Email = request.Email,
+                PhoneNumber = request.PhoneNumber,
+                Address = request.Address,
+                City = request.City,
+                State = request.State,
+                PostalCode = request.PostalCode,
+                Country = request.Country,
+                GuardianName = request.GuardianName,
+                GuardianPhone = request.GuardianPhone,
+                GuardianEmail = request.GuardianEmail,
+                RelationshipToStudent = request.RelationshipToStudent,
+                IsActive = request.IsActive,
+                CreatedAt = request.CreatedAt,
+                UpdatedAt = request.UpdatedAt,
+            };
+            return await _studentRepository.UpdateAsync(request.Id, studentEntity);
+        }
+    }
+}
