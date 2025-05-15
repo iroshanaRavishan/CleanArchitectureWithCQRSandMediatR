7bd6caf22c03a9df9618662549747693ea453324
IroshanaRavishan <bandarairoshana@gmail.com>
2025-05-15 22:41:29 +0530

    added the functionality of getting student by Email

---
+++ CleanArchitectureWithCQRSandMediatR.Application/Students/Queries/GetStudentByEmail/GetStudentByEmailQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitectureWithCQRSandMediatR.Application.Students.Queries.GetStudentByEmail
+{
+    public class GetStudentByEmailQuery : IRequest<StudentViweModel>
+    {
+        public string Email { get; set; }
+    }
+}
